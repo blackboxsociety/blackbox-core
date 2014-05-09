aa17136037ebacb4c1ef09da16f35a49cf35599a
Joseph Moniz <joseph.moniz@gmail.com>
2014-05-08 20:54:35 -0700

    restored finishable

--- src/main/scala/com/blackboxsociety/util/Finishable.scala
+++ src/main/scala/com/blackboxsociety/util/Finishable.scala
@@ -1,5 +1,25 @@
 package com.blackboxsociety.util
 
-sealed trait Finishable[A]
+import scalaz.Functor
+
+sealed trait Finishable[A] {
+  val value: A
+}
+
 case class More[A](value: A) extends Finishable[A]
 case class Done[A](value: A) extends Finishable[A]
+
+object Finishable {
+
+  implicit val FinishableFunctor = new Functor[Finishable] {
+    override def map[A, B](fa: Finishable[A])(f: (A) => B): Finishable[B] = fa match {
+      case More(m) => More(f(m))
+      case Done(d) => Done(f(d))
+    }
+  }
+
+  def more[A](a: A): Finishable[A] = More(a)
+
+  def done[A](a: A): Finishable[A] = Done(a)
+
+}
